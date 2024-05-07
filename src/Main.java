a29a8fd69cd5facbc070b4a045fc949a07cd4b59
Pabl0ski <167172792+Pabl0ski@users.noreply.github.com>
2024-05-07 09:20:48 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hola mundo, pero en español");
+        System.out.println("Otro cambio, pero esta vez para generar un conflicto");
     }
 }
